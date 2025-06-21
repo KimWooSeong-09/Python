118e7d951feaec23164aed4f1f9e24b72b8577b3
Macbook <macbook@Macbookui-MacBookPro.local>
2025-06-21 21:14:28 +0900

    WIP 제거, 최종 작업 완료

---
+++ 250317/13-1405김우성.py
@@ -0,0 +1,3 @@
+print('#'*20)
+n = list(map(str, input().split()))
+print(f'중복 제외: {set(n)}')
--- test.py
+++ test.py
@@ -1,4 +1,25 @@
-a = list(range(0, 10))
-a = str(a)
+import random as r
 
-print(a)
+user_input = list(map(int, input("숫자 3개 입력 (1~9) : ").split()))
+random_list = r.sample(range(1, 10), 3)
+
+S = 0  # 스트라이크 개수
+B = 0  # 볼 개수
+
+# 스트라이크 판별
+if user_input[0] == random_list[0]:
+    S += 1
+if user_input[1] == random_list[1]:
+    S += 1
+if user_input[2] == random_list[2]:
+    S += 1
+
+# 볼 판별 (같은 숫자가 있지만 위치가 다를 경우)
+if user_input[0] in random_list and user_input[0] != random_list[0]:
+    B += 1
+if user_input[1] in random_list and user_input[1] != random_list[1]:
+    B += 1
+if user_input[2] in random_list and user_input[2] != random_list[2]:
+    B += 1
+
+print(f'컴퓨터 리스트 : {random_list}, S : {S}, B : {B}')
